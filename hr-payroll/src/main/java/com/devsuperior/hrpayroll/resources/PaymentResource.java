cd5a9607a2cd53d5088f0f37ab03ee3866beb21f
Diogo Felix <diogocarpio@brq.com>
2021-10-04 15:56:37 -0300

    RestTemplate

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/resources/PaymentResource.java
@@ -18,7 +18,7 @@ public class PaymentResource {
 	@Autowired
 	private PaymentService service;
 	
-	@GetMapping(value = "/{workerdId}/days/{days}")
+	@GetMapping(value = "/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
 		Payment payment = service.getPayment(workerId, days);
 		return ResponseEntity.ok(payment);	
